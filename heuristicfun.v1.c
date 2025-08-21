e3fc6738b78213b50845a3ea6bdbee4d57580a33
Neelabja Chandra Ghosh <neelabjachandrag@gmail.com>
2025-08-21 20:57:04 +0530

    Add files via upload

---
+++ heuristicfun.v1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+char board[3][3]={{'o','o','x'},
+                  {'o','x','\0'},
+                  {'o','\0','\0'}};
+                  
+//heuristic function
+int check(char board[][3],int depth){
+    for(int i=0;i<3;i++){
+    //row & column wise
+    if((board[i][0]==board[i][1] && board[i][1]==board[i][2] && board[i][0]=='x')||(board[0][i]==board[1][i] && board[1][i]==board[2][i] && board[0][i]=='x')){
+        return 10;
+      }
+    if((board[i][0]==board[i][1] && board[i][1]==board[i][2] && board[i][0]=='o')||(board[0][i]==board[1][i] && board[1][i]==board[2][i] && board[0][i]=='o')){
+        return -10;
+      }
+    }
+   //checking diagonals
+    if((board[0][0]==board[1][1] && board[1][1]==board[2][2] && board[1][1]=='x')||(board[0][2]==board[1][1] && board[1][1]==board[2][0] && board[1][1]=='x')){
+        return 10;
+      }
+    if((board[0][0]==board[1][1] && board[1][1]==board[2][2] && board[1][1]=='o')||(board[0][2]==board[1][1] && board[1][1]==board[2][0] && board[1][1]=='o')){
+        return -10;
+      }
+  return 0;
+}
