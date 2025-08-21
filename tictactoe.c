a026ead0de6659ebb20fb1910c6a4a9c318fd74e
Neelabja Chandra Ghosh <neelabjachandrag@gmail.com>
2025-08-21 21:16:51 +0530

    Update tictactoe.c

--- tictactoe.c
+++ tictactoe.c
@@ -10,7 +10,7 @@ struct move{
   int row,col;
 }move;
 
-//draw checker
+//checks for empty cells 
 bool isFull(char board[][3]){
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
@@ -234,4 +234,5 @@ else{
   }
 
 }
+
 
